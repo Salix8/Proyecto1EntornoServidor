6afb749d654a08a254506d1591a9da5c6517a10d
Salix8 <saulpachecotrilles@gmail.com>
2021-10-18 10:40:29 +0200

    Empezando v3

---
+++ proyecto1 photo-master/views/partials/imagen-galeria.part.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @var int
+ * Genera el id de la categoría en el div correspondiente //1, 2 o 3
+ */
+$idCategory;
+
+/**
+ * @var bool
+ * Indica si la categoria esta activa o no
+ */
+$isActive = false;
+
+/**
+ * @var array
+ * Genera codigo de cada imagen a partir de los mismos datos
+ */
+$ImagenGallery;
+
+
+foreach ($ImagenGallery as $key => $value) {
+    
+}
+
