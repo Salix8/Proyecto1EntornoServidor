c2917506784553bea7878eea544362127c89bca3
Salix8 <saulpachecotrilles@gmail.com>
2021-10-20 10:43:10 +0200

    v4 empezando

--- contact.php
+++ contact.php
@@ -5,3 +5,19 @@
     require_once "./utils/utils.php";
 
     include("./views/contact.view.php");
+
+    $firstName = $lastName = $email = $subject = $message = "";
+    $firstNameError = $emailErr = $subjectError = $hayErrores = false;
+    $errores = [];
+
+    if ("POST" === $_SERVER["REQUEST_METHOD"]) {
+        //Nunca confiar en que llegan todos los datos!!
+        $firstName = sanitizeInput($_POST["firstName"] ?? "");
+        $lastName = sanitizeInput($_POST["lastName"] ?? ""); //Campo opcional
+        $email = sanitizeInput($_POST["email"] ?? "");
+        $subject = sanitizeInput($_POST["subject"] ?? "");
+        $message = sanitizeInput($_POST["message"] ?? ""); //Campo opcional
+        //ahora hacer las comprobaciones
+    }
+
+    
--- utils/utils.php
+++ utils/utils.php
@@ -7,6 +7,7 @@ function esOpcionMenuActiva(string $option): bool{
     }else   
         return false;
 }
+
 function  existeOpcionMenuActivaEnArray(array $options): bool{
     foreach ($options as $option){
         if (esOpcionMenuActiva($option)) {
@@ -14,4 +15,14 @@ function  existeOpcionMenuActivaEnArray(array $options): bool{
         }
     }
     return false;
+}
+
+function sanitizeInput(string $data): string {
+    $data = trim($data);
+    //Quitar las comillas escampadas \' y \""
+    $data = stripslashes($data);
+    //Prevenir la introduccion de scripts en los campos
+    $data = htmlspecialchars($data);
+
+    return $data;
 }
