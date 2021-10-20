bde9fb93da2b51a479dbc7843bd85879ad82368c
Salix8 <saulpachecotrilles@gmail.com>
2021-10-20 09:50:09 +0200

    v3 acabada

--- utils/utils.php
+++ utils/utils.php
@@ -1,17 +1,16 @@
-<?php 
+<?php
 function esOpcionMenuActiva(string $option): bool{
-    if (strpos($_SERVER["REQUEST_URI"], $option)) {
+    if (strpos($_SERVER["REQUEST_URI"], "/". $option) === 0 ){
         return true;
-    }elseif (strpos($_SERVER["REQUEST_URI"], "/")) {
+    }elseif ("/" === $_SERVER["REQUEST_URI"] && ("index" == $option)){
+        //tal vez hayamos entrado de forma directa, sin index.php
         return true;
-    }else{
+    }else   
         return false;
-    }
 }
-
 function  existeOpcionMenuActivaEnArray(array $options): bool{
-    foreach ($options as $value) {
-        if (esOpcionMenuActiva($value) !== false) {
+    foreach ($options as $option){
+        if (esOpcionMenuActiva($option)) {
             return true;
         }
     }
