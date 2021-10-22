5007f31a8719efe85b8c78be7526d2209bb1861b
Salix8 <saulpachecotrilles@gmail.com>
2021-10-22 08:54:28 +0200

    v4 acabada

--- contact.php
+++ contact.php
@@ -3,10 +3,9 @@
     $title = "Contact";
 
     require_once "./utils/utils.php";
+    
 
-    include("./views/contact.view.php");
-
-    $firstName = $lastName = $email = $subject = $message = "";
+    $info = $firstName = $lastName = $email = $subject = $message = "";
     $firstNameError = $emailErr = $subjectError = $hayErrores = false;
     $errores = [];
 
@@ -18,6 +17,32 @@
         $subject = sanitizeInput($_POST["subject"] ?? "");
         $message = sanitizeInput($_POST["message"] ?? ""); //Campo opcional
         //ahora hacer las comprobaciones
+
+        if (empty($firstName)) {
+            $errores[] = "El nombre es obligatorio";
+            $firstNameError = true;
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errores[] = "Formato inválido de correo";
+            $emailErr = true;
+        }
+        if (empty($subject)) {
+            $errores[] = "El asunto es obligatorio";
+            $firstNameError = true;
+        }
+        if (sizeOf($errores)) {
+            $hayErrores = true;
+        }
+        if (!$hayErrores) {
+            //En este caso todo ha ido bien. En un caso real insertariamos el mensaje de la base de datos
+            //Además, en este caso vamos amostrar un mensaje al usuario indicando que todo ha ido bien
+            $info = "Mensaje insertado correctamente:";
+            //Reseteamos los datos del formulario
+            $firstName = $lastName = $email = $subject = $message = "";
+            //Insertarlo en la base de datos
+        }else{
+            $info = "Datos erróneos";
+        }
     }
 
-    
+    include("./views/contact.view.php");
